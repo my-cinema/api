459bf37efd2ee7954a4cd08da5e78b54fbac9219
waveaway77 <waveaway77@gmail.com>
2024-09-16 01:03:55 +0900

    local init

---
+++ src/test/java/org/inin/mycinema/MyCinemaApplicationTests.java
@@ -0,0 +1,13 @@
+package org.inin.mycinema;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyCinemaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
